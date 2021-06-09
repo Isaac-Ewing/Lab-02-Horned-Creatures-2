0904c06a0e0731d830dd37320d6b4dde977a00fc
Isaac <isaac.ewing3@gmail.com>
2021-06-09 16:43:35 -0600

    fun debugging

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import data from './data.js';
 import './App.css';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
+import Dropdown from './Dropdown.js';
 
 export default class App extends React.Component {
   state = {
@@ -10,7 +11,10 @@ export default class App extends React.Component {
     horns: '',
   }
   handleChange = (e) => {
-    this.setState({keyword: e.target.value});
+    let keys = '';
+    if (e.target.value !== '')
+      keys = String.fromCharCode(e.target.value[0].charCodeAt(0) + 32) + e.target.value.substring(1, e.target.value.length)
+    this.setState({keyword: keys});
   }
   handleHornChange = (e) => {
     this.setState({horns: e.target.value});
@@ -20,40 +24,17 @@ export default class App extends React.Component {
     if (this.state.keyword !== '') {
       pics = pics.filter(animal => animal.keyword === this.state.keyword);
     }
-    if (this.state.horns !== '' && this.state.horns !== '4') {
+    if (this.state.horns !== '') {
       pics = pics.filter(item => Number(item.horns) === Number(this.state.horns));
     }
-    if (this.state.horns === '4') {
-      pics = pics.filter(item => Number(item.horns) >= Number(this.state.horns));
-    }
+    let animalOptions = Array.from(new Set(data.map(data => data.keyword)));
+    animalOptions = animalOptions.map(animal => (String.fromCharCode(animal[0].charCodeAt(0) - 32) + animal.substring(1, animal.length)))
+    const hornsOptions = Array.from(new Set(data.map(data => data.horns)));
     return (
       <div className="App">
         <Header/>
-        <label>
-          <select onChange={this.handleChange}>
-            <option value="">--select--</option>
-            <option value="narwhal">Narwhal</option>
-            <option value="rhino">Rhino</option>
-            <option value="unicorn">Unicorn</option>
-            <option value="unilego">Unilego</option>
-            <option value="triceratops">Triceratops</option>
-            <option value="markhor">Markhor</option>
-            <option value="mouflon">Mouflon</option>
-            <option value="addax">Addax</option>
-            <option value="chameleon">Chameleon</option>
-            <option value="lizard">Lizard</option>
-            <option value="dragon">Dragon</option>
-          </select>
-        </label>
-        <label>
-          <select onChange={this.handleHornChange}>
-            <option value="">--select--</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4+</option>
-          </select>
-        </label>
+        <Dropdown options={animalOptions} handleChange={this.handleChange}/>
+        <Dropdown options={hornsOptions} handleChange={this.handleHornChange}/>
         <div>
           <ImageList images={pics}/>
         </div>
