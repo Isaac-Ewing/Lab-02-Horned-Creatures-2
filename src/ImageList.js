dd73b412a5c6474fb8742ac2202c38b74616d625
Isaac <isaac.ewing3@gmail.com>
2021-06-09 16:58:56 -0600

    added titles

--- src/ImageList.js
+++ src/ImageList.js
@@ -5,7 +5,7 @@ export default class ImageList extends React.Component {
     render() {
         return (
             <div id="images">
-                {this.props.images.map(data => <ImageItem url={data.url} key={data.description} description={data.description}/>)}
+                {this.props.images.map(data => <ImageItem title = {data.title}url={data.url} key={data.description} description={data.description}/>)}
             </div>
         );
     }
